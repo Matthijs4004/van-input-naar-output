78da3d6091a90ebdcd057f9b11a564589fa29b7b
Matthijs Raatgever <matthijs4004@gmail.com>
2021-09-06 15:46:10 +0200

    person

---
+++ person.py
@@ -0,0 +1,13 @@
+
+name = input("Naam: ")
+adres = input("Adres: ")
+postcode = input("Postcode: ")
+woonplaats = input("Woonplaats: ")
+
+
+print("----------------------------------------------------")
+print("|  Naam      : " + name)               
+print("|  Adres     : " + adres)      
+print("|  Postcode  : " + postcode)                      
+print("|  Woonplaats: " + woonplaats)                        
+print("----------------------------------------------------")
